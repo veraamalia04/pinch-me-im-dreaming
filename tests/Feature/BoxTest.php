2ea338d2acb3402ef49c65ea0ab1329ad0d9cc8a
veerazayn <amaliarara152@gmail.com>
2026-07-28 19:05:47 +0700

    Membuat Testing Box

---
+++ tests/Feature/BoxTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Box;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class BoxTest extends TestCase
+{
+    public function test_menambahkan_ke_box(){
+        $user = User::factory()->create([
+            'username' => 'zayn',
+            'password' => Hash::make('password'),
+        ]);
+        
+        $user->roles()->sync([1,2,3]);
+
+        $this->actingAs($user);
+
+        $product = Product::factory()->create();
+
+        $response = $this->post(route('post.box.store_to_box'), [
+            'product_id' => $product->id,
+            'quantity' => 20,
+        ]);
+
+        $response->assertRedirectBack();
+
+        $this->assertDatabaseHas('boxes', [
+            'user_id' => $user->id,
+        ]);
+
+        $this->assertDatabaseHas('box_details', [
+            'product_id' => $product->id,
+            'quantity' => 20,
+        ]);
+    }
+
+    public function test_menghapus_item_dari_box(){
+        $user = User::factory()->create([
+            'username' => 'zayn',
+            'password' => Hash::make('password'),
+        ]);
+        
+        $user->roles()->sync([1,2,3]);
+
+        $this->actingAs($user);
+
+        $product = Product::factory()->create();
+
+        $response = $this->post(route('post.box.store_to_box'), [
+            'product_id' => $product->id,
+            'quantity' => 20,
+        ]);
+
+        $box = Box::with('details')->where('user_id', $user->id)->first();
+
+        $boxDetail = $box->details[0];
+
+        $response = $this->delete(route('delete.box.delete_box_detail', $boxDetail->id));
+
+        $response->assertRedirectBack();
+
+        $this->assertDatabaseMissing('box_details', [
+            'product_id' => $product->id,
+        ]);
+    }
+
+    public function test_memesan_dari_box(){
+        $user = User::factory()->create([
+            'username' => 'zayn',
+            'password' => Hash::make('password'),
+        ]);
+        
+        $user->roles()->sync([1,2,3]);
+
+        $this->actingAs($user);
+
+        $product = Product::factory()->create();
+
+        $response = $this->post(route('post.box.store_to_box'), [
+            'product_id' => $product->id,
+            'quantity' => 20,
+        ]);
+
+        $response->assertRedirectBack();
+
+        $this->assertDatabaseHas('boxes', [
+            'user_id' => $user->id,
+        ]);
+
+        $this->assertDatabaseHas('box_details', [
+            'product_id' => $product->id,
+            'quantity' => 20,
+        ]);
+
+        $box = Box::with('details')->where('user_id', $user->id)->first();
+        $boxDetail = $box->details[0];
+
+        $response = $this->post(route('post.order.transfer_box_to_order'));
+
+        $this->assertDatabaseHas('order_details', [
+            'product_id' => $product->id,
+            'quantity' => 20,
+        ]);
+
+        $this->assertDatabaseHas('orders', [
+            'user_id' => $user->id,
+            'pemesanan_pada' => now(),
+        ]);
+    }
+
+
+}
